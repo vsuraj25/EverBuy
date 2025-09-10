3e3e91c0cfbb3b03995929ee3a8e8d89463ffecb
vsuraj25 <stdmail2552@gmail.com>
2025-09-11 00:44:07 +0530

    fix: added meta class to change the verbose name for the category app in the admin panel

--- category/models.py
+++ category/models.py
@@ -8,5 +8,9 @@ class Category(models.Model):
     description = models.TextField(max_length=255)
     category_image = models.ImageField(upload_to='photos/categories', blank= True)
 
+    class Meta():
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
     def __str__(self):
         return self.category_name

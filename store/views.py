9718a6b2430798efb1381a6504084070fc39ff30
vsuraj25 <stdmail2552@gmail.com>
2025-09-13 10:51:24 +0530

    feat: added store template, dynamic data logic and url redirection to the store page

--- store/views.py
+++ store/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product 
 
-# Create your views here.
+def store(request):
+    
+    products = Product.objects.all().filter(is_available =True)
+
+    context = {
+        'products': products,
+        'products_count': len(products)
+    }
+
+    return render(request, 'store/store.html', context)

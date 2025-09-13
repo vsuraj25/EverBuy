8cb21015ad27e54bfe7054c430b146641e0df609
vsuraj25 <stdmail2552@gmail.com>
2025-09-13 11:06:36 +0530

    feat: added slug for the store categories and filtering the data as per the given category

--- store/views.py
+++ store/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-from .models import Product 
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from category.models import Category
 
-def store(request):
+def store(request, category_slug= None):
     
-    products = Product.objects.all().filter(is_available =True)
+    categories = None
+    products = None
+
+    if category_slug:
+        categories = get_object_or_404(Category, slug = category_slug)
+        products = Product.objects.filter(category = categories, is_available = True)
+    else:
+        products = Product.objects.all().filter(is_available = True)
+        
+    # products = Product.objects.all().filter(is_available =True)
 
     context = {
         'products': products,

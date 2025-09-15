3c48da2a2c313850682b289d937922fe928f2063
vsuraj25 <stdmail2552@gmail.com>
2025-09-15 16:57:25 +0530

    feat: added product_detail view for product detail page

--- store/views.py
+++ store/views.py
@@ -21,3 +21,14 @@ def store(request, category_slug= None):
     }
 
     return render(request, 'store/store.html', context)
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug = category_slug, slug = product_slug)
+    except Exception as e:
+        raise e
+    
+    context = {
+        'product': product
+    }
+    return render(request, 'store/product_detail.html', context)

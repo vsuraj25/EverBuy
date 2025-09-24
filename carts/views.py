1278ce3d059adce5e7b9d6ab16d1da276138ebcd
vsuraj25 <stdmail2552@gmail.com>
2025-09-25 03:01:56 +0530

    feat: added add_product_to_cart method to handle cart item addtions and other operations in the cart page

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from store.models import Product
+from carts.models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
+
+def _cart_id(request):
+    cart = request.session.session_key
+    if not cart:
+        cart = request.session.create()
+    return cart
 
 def cart(request):
-    return render(request, 'store/cart.html')
+    try:
+        cart = Cart.objects.get(cart_id= _cart_id(request))
+        cart_items = CartItem.objects.filter(cart = cart, is_active =True)
+        total, quantity = 0, 0
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.cart_quantity)
+            quantity += cart_item.cart_quantity
+
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        'total' : total,
+        'quantity': quantity,
+        'cart_items': cart_items
+    }
+
+    return render(request, 'store/cart.html', context)
+
+def add_product_to_cart(request, product_id):
+
+    ## Getting the requested product ID
+    product = Product.objects.get(id=product_id)
+
+    try:
+        ## Getting the cart id i.e. session_id
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(
+            cart_id = _cart_id(request)
+        )
+
+    cart.save()
+
+    try:
+        cart_item = CartItem.objects.get(product=product, cart=cart)
+        cart_item.cart_quantity += 1
+        cart_item.save()
+
+    except CartItem.DoesNotExist:
+        cart_item = CartItem.objects.create(
+            product=product, 
+            cart=cart, 
+            cart_quantity = 1
+        )
+
+        cart_item.save()
+
+    return redirect('cart')

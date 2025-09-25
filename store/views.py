23e81f75240f779efb8c0ec9781e52205bb8c580
vsuraj25 <stdmail2552@gmail.com>
2025-09-25 14:24:34 +0530

    feat: added paginator in the store products list page

--- store/views.py
+++ store/views.py
@@ -3,6 +3,7 @@ from .models import Product
 from carts.models import CartItem
 from carts.views import _cart_id
 from category.models import Category
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def store(request, category_slug= None):
     
@@ -13,12 +14,15 @@ def store(request, category_slug= None):
         categories = get_object_or_404(Category, slug = category_slug)
         products = Product.objects.filter(category = categories, is_available = True)
     else:
-        products = Product.objects.all().filter(is_available = True)
+        products = Product.objects.all().filter(is_available = True).order_by('id')
+
         
-    # products = Product.objects.all().filter(is_available =True)
+    paginator = Paginator(products, 6)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
 
     context = {
-        'products': products,
+        'products': paged_products,
         'products_count': len(products)
     }
 

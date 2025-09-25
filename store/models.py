7efa19c39c96558188ac4f6e8766f81b37648fee
vsuraj25 <stdmail2552@gmail.com>
2025-09-25 18:03:40 +0530

    feat: added variation manager to dynamically add the variations

--- store/models.py
+++ store/models.py
@@ -24,6 +24,13 @@ class Product(models.Model):
     def get_url(self):
         return reverse('product_detail', args = [self.category.slug, self.slug])
     
+class VariationManager(models.Manager):
+    def colors(self):
+        return super(VariationManager, self).filter(variation_category='color', is_active = True)
+
+    def sizes(self):
+        return super(VariationManager, self).filter(variation_category='size', is_active =True)
+    
 class Variation(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     variation_category = models.CharField(max_length=100, choices=variation_category_choices)
@@ -31,7 +38,10 @@ class Variation(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    objects = VariationManager()
+
     def __unicode__(self):
         return self.product
+
     
 

257a5a5af52f2aec37aed01a1deeaedb63e63795
vsuraj25 <stdmail2552@gmail.com>
2025-09-25 12:46:55 +0530

    feat: added remove cart item feature to remove the items from the cart

--- carts/urls.py
+++ carts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.cart, name ='cart'),
     path('add_cart/<int:product_id>/', views.add_product_to_cart, name = 'add_product_to_cart'),
-    path('remove_cart/<int:product_id>/', views.remove_product_from_cart, name = 'remove_product_from_cart')
+    path('remove_cart/<int:product_id>/', views.remove_product_from_cart, name = 'remove_product_from_cart'),
+    path('remove_cart_item/<int:product_id>/', views.remove_cart_item, name = 'remove_cart_item'),
 ]

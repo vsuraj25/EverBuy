00f112ad0cb65a58b8afd6d26df02fcd39d2e711
vsuraj25 <stdmail2552@gmail.com>
2025-09-27 01:59:27 +0530

    feat: added django forms to the html and updated views

---
+++ accounts/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Account
+
+class RegistrationForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Enter Password", 'class': 'form-control' 
+    }))
+
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Confirm Password"
+    }))
+
+    class Meta:
+        model = Account
+        fields = ('first_name', 'last_name', 'phone', 'email', 'password')
+        
+    ## Overriding the placeholder and class of fields from djago registration forms
+    def __init__(self, *args, **kwargs):
+
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['phone'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email'
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'

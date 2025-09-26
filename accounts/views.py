524e67ecbfe98e87096b71cfc2e833f158d9e67a
vsuraj25 <stdmail2552@gmail.com>
2025-09-27 02:32:32 +0530

    feat: implemented registration form functionality and saving the data in the accounts table

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from .forms import RegistrationForm
+from .models import Account
 
 def register(request):
-    form = RegistrationForm()
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            first_name =  cleaned_data['first_name']
+            last_name =  cleaned_data['last_name']
+            phone =  cleaned_data['phone']
+            email =  cleaned_data['email']
+            password =  cleaned_data['password']
+            username = email.split('@')[0]
+
+            user = Account.objects.create_user(first_name= first_name, last_name=last_name,
+                     email =email, username =username, password = password)
+            user.phone = phone
+            user.save()
+    else:
+        form = RegistrationForm()
+
     context = {
         'form': form
     }
